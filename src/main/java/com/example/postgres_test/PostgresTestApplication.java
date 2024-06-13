ec8ca5c50606a619dd882453bbfb61887eb815f8
Grischakrombacha <grischa.storch@gmail.com>
2024-06-13 15:08:02 +0200

    Main Protection Test 2

--- src/main/java/com/example/postgres_test/PostgresTestApplication.java
+++ src/main/java/com/example/postgres_test/PostgresTestApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PostgresTestApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello!");
+		System.out.println("H");
 		SpringApplication.run(PostgresTestApplication.class, args);
 	}
 
